b92479ef0e6d648ea64bf2c0625fc5f9e47137e2
Raimundo Oyarce Andrade <82007842+mumoyarce96@users.noreply.github.com>
2024-08-24 20:41:54 -0400

    Update update_fantasy_scores.py

--- update_fantasy_scores.py
+++ update_fantasy_scores.py
@@ -9,7 +9,13 @@ try:
 except:
   previous_scores = pd.DataFrame(columns = ['match_id', 'player_name',	'team',	'final_score',	'position',	'season_id',	'round'])
 importances_df = pd.read_csv('data/importancia_stats_por_posicion.csv')
+matches_df = pd.read_parquet(f'data/Matches/{tournament_id}_matches.parquet')
+
+final_output = previous_scores.copy()
 
 for position in previous_scores['position']:
-   calculate_final_scores(player_stats_df, player_positions_df, previous_scores, importances_df, position)
+   temp_df = fantasy_scores_output(player_stats_df, player_positions_df, previous_scores, importances_df, matches_df, position)
+   final_output = pd.concat([final_output, temp_df])
+
+final_output.to_parquet(f'data/Fantasy Scores/{tournament_id}_fantasy_scores.parquet')
 
