7dc09ab8ba4908c0b36ba52b4dbd2dc088c4f3c7
Raimundo Oyarce Andrade <82007842+mumoyarce96@users.noreply.github.com>
2024-08-24 21:20:38 -0400

    Update update_fantasy_scores.py

--- update_fantasy_scores.py
+++ update_fantasy_scores.py
@@ -5,7 +5,7 @@ tournament_id = 11653
 player_stats_df = pd.read_parquet(f'data/Player Stats/{tournament_id}_player_stats.parquet')
 player_positions_df =  pd.read_parquet(f'data/Player Positions/{tournament_id}_player_positions.parquet')
 try:
-  previous_scores = pd.read_parquet(f'data/Fantasy Scores/{tournament_id}_fantasy_scores.parquet')
+  previous_scores = pd.read_parquet(f'data/Fantasy Scores/{tournament_id}_player_fantasy_scores.parquet')
 except:
   previous_scores = pd.DataFrame(columns = ['match_id', 'player_name',	'team',	'final_score',	'position',	'season_id',	'round'])
 importances_df = pd.read_csv('data/importancia_stats_por_posicion.csv')
@@ -17,5 +17,5 @@ for position in previous_scores['position']:
    temp_df = fantasy_scores_output(player_stats_df, player_positions_df, previous_scores, importances_df, matches_df, position)
    final_output = pd.concat([final_output, temp_df])
 
-final_output.to_parquet(f'data/Fantasy Scores/{tournament_id}_fantasy_scores.parquet')
+final_output.to_parquet(f'data/Fantasy Scores/{tournament_id}_player_fantasy_scores.parquet')
 
